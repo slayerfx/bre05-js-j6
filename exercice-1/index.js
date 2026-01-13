96c45b13a3fbedd3602abe91524929c0e419ca59
Louenn Penanc’hoat <louenn.p@gmail.com>
2026-01-13 11:23:34 +0100

    Update

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -1,7 +1,7 @@
-const loginForm = document.getElementById("loginForm");
+const loginForm = document.querySelector("#loginForm");
 
 loginForm.addEventListener("submit", function (event) {
-  event.preventDefault(); 
+  event.preventDefault();
   console.log(event.target.elements.username.value);
   console.log(event.target.elements.password.value);
 });
--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,7 +1,8 @@
-const loginForm = document.getElementById("loginForm");
+const loginForm = document.querySelector("#loginForm");
 
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
+
   console.log(event.target.elements.username.value);
   console.log(event.target.elements.password.value);
   event.target.submit();
